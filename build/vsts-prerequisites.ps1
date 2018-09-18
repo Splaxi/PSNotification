476d88252071d7f685ea8d734903647b5b7373b9
Mötz Jensen <Motz@e-s.local>
2018-09-18 22:08:23 +0200

    Extend the filesAfter to contain the new variables.ps1 and updated the build prereq with PoshRSJob and PSScriptAnalyzer

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -2,3 +2,7 @@
 Install-Module Pester -Force -SkipPublisherCheck
 Write-Host "Installing PSFramework" -ForegroundColor Cyan
 Install-Module PSFramework -Force -SkipPublisherCheck
+Write-Host "Installing PoshRSJob" -ForegroundColor Cyan
+Install-Module PoshRSJob -Force -SkipPublisherCheck
+Write-Host "Installing PSScriptAnalyzer" -ForegroundColor Cyan
+Install-Module PSScriptAnalyzer -Force -SkipPublisherCheck
